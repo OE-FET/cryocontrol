c57fddb5f05e86b7474478d48ae5631cc784fef7
Sam Schott <ss2151@cam.ac.uk>
2020-08-25 12:47:42 +0200

    fixup

--- cryocontrol/config/user.py
+++ cryocontrol/config/user.py
@@ -388,19 +388,6 @@ class UserConfig(DefaultsConfig):
             value = float(value)
         elif isinstance(default_value, int):
             value = int(value)
-        elif is_text_string(default_value):
-            try:
-                value = value.decode('utf-8')
-                try:
-                    # Some str config values expect to be eval after
-                    # decoding
-                    new_value = ast.literal_eval(value)
-                    if is_text_string(new_value):
-                        value = new_value
-                except (SyntaxError, ValueError):
-                    pass
-            except (UnicodeEncodeError, UnicodeDecodeError):
-                pass
         else:
             try:
                 # lists, tuples, ...
